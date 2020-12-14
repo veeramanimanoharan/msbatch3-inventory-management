756d59375484bd9991886e48fdf71b43cce8db06
veeramanimanoharan <veeramanimanoharan@outlook.com>
2020-12-14 12:06:47 +0530

    Put Working

--- src/main/java/com/sl/ms/inventorymanagement/controller/ProductController.java
+++ src/main/java/com/sl/ms/inventorymanagement/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -70,5 +71,14 @@ public class ProductController {
 		proService.delete(id);
 		return tt;
 	}
-
+	@PutMapping("/products/{product_id}")
+	private Product updateproduct(@PathVariable("product_id") int id, @RequestBody Product product1) {
+		Product product = proService.getById(id);
+		product.setName(product1.getName());
+		product.setPrice(product1.getPrice());
+		product.setQuantity(product1.getQuantity());
+		proService.save(product);
+		return product;
+		
+	}
 }
