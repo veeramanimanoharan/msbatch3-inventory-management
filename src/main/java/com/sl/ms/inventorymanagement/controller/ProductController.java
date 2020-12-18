449bf83e2024f50963bed012c1bd8c711a9e1f2e
veeramanimanoharan <65152330+veeramanimanoharan@users.noreply.github.com>
2020-12-18 23:48:40 +0530

    MutiFile upload working

--- src/main/java/com/sl/ms/inventorymanagement/controller/ProductController.java
+++ src/main/java/com/sl/ms/inventorymanagement/controller/ProductController.java
@@ -1,20 +1,28 @@
 package com.sl.ms.inventorymanagement.controller;
 
+
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -98,28 +106,59 @@ public class ProductController {
 	//****************************************************************************
 
 	@GetMapping("/inv")
-	private void tttt()
+	private List<Inventory> getInvList()
 	{
-		Inventory inv = new Inventory();
-		inv.setDate(LocalDateTime.now());
-		inv.setSample("sample");
-		invService.save(inv);
+//		Inventory inv = new Inventory();
+//		inv.setDate(LocalDateTime.now());
+//		inv.setSample("sample");
+//		invService.save(inv);
+		List<Inventory> invlist;
+		invlist=invService.getAllInventory();
+		return invlist;
 	}
 	
 	
-	@GetMapping("/upload/csv")
-	private void ttt() throws IOException 
+	@PostMapping("/upload/csvFiles")
+	private String uploadMultiFile(@RequestParam("file") MultipartFile[] files) throws IOException  
 	{
+//	System.out.println(files.length+ "    ");
 	
-	readcsv();
+	for (int i = 0; i < files.length; i++) {
+		MultipartFile file = files[i];
+
+		Path filePath = Paths.get(System.getProperty("java.io.tmpdir"), file.getOriginalFilename());
+
+//		System.out.println(filePath);
+		file.transferTo(filePath);
+		readcsv(filePath.toString());
+		File filed = new File(filePath.toString()); 
+		filed.delete();
 	}
-	private  void readcsv() throws  IOException {
-//		System.out.println("Veera1");
-		
 	
+			return "File Uploaded sucessfully";
+	
+	}
+	
+
+	@PostMapping("/upload/csv")
+	private String uploadSingleFile(@RequestParam("file") MultipartFile file) throws IOException 
+	{
+
+		Path filePath = Paths.get(System.getProperty("java.io.tmpdir"), file.getOriginalFilename());
+
+//		System.out.println(filePath);
+		file.transferTo(filePath);
+//		System.out.println(filePath.toString());
+		readcsv(filePath.toString());
+
+		return "File Uploaded sucessfully";
+	
+	}
+	private  void readcsv(String csvfile) throws  IOException {
+
 		CSVReader reader;
 		try {
-			reader = new CSVReader(new FileReader("C:\\samp_inv.csv"), ',');
+			reader = new CSVReader(new FileReader(csvfile), ',');
 		
 
 		List<Product> prods = new ArrayList<Product>();
@@ -127,7 +166,7 @@ public class ProductController {
 		// read line by line
 		String[] record = null;
 		record = reader.readNext();
-
+//
 		while ((record = reader.readNext()) != null) {
 			Product pro = new Product();
 			pro.setId(Integer.parseInt(record[0]));
@@ -140,15 +179,15 @@ public class ProductController {
 			
 		}
 
-		System.out.println(prods);
+//		System.out.println(prods);
 		proService.savelist(prods);
 		
 		ObjectMapper objectMapper = new ObjectMapper();
         //Set pretty printing of json
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         String arrayToJson = objectMapper.writeValueAsString(prods);
-        System.out.println("1. Convert List of person objects to JSON :");
-        System.out.println(arrayToJson);
+//        System.out.println("1. Convert List of person objects to JSON :");
+//        System.out.println(arrayToJson);
         
         
 		Inventory inv = new Inventory();
